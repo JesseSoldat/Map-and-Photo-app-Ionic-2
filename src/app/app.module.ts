e5570be36beef9ec459ffc101314b451ea187cfb
jesse <jsoldat@hotmail.com>
2017-05-17 17:44:12 -0400

    generated pages and added models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,36 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddPlacePage } from "../pages/add-place/add-place";
+import { PlacePage } from "../pages/place/place";
+import { SetLocationPage } from "../pages/set-location/set-location";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC3q7NMdCJQI6-uUDf39e4YUepsNawbM78'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { AddPlacePage } from '../add-place/add-place';
+import { Place } from '../../models/place';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	addPlacePage = AddPlacePage;
+	places: Place[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
 
   }
 
